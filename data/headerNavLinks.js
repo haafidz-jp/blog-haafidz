66af709a81444bf0fffdcb42b533e5533fa86234
haafidz-jp <haafidz123@gmail.com>
2022-01-25 10:42:28 +0700

    update navlink

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: 'https://stream.haafidz123.workers.dev', title: 'Stream' },
+  { href: 'https://stream2.haafidz123.workers.dev', title: 'Stream 2' },
   { href: '/about', title: 'About' },
 ]
 
